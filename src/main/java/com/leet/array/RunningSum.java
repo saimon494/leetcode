3669762cfbde09d46e53a292275eb650395f575e
saimon494 <saimon.494@gmail.com>
2021-05-19 20:50:46 +0300

    Array - Running Sum of 1d Array

---
+++ src/main/java/com/leet/array/RunningSum.java
@@ -0,0 +1,17 @@
+package com.leet.array;
+
+// Given an array nums. We define a running sum of an array
+// as runningSum[i] = sum(nums[0]…nums[i]).
+// Return the running sum of nums.
+
+public class RunningSum {
+
+    public int[] runningSum(int[] nums) {
+        int[] rsl = new int[nums.length];
+        rsl[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            rsl[i] = rsl[i - 1] + nums [i];
+        }
+        return rsl;
+    }
+}
